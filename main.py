efd48f3aea6f491d018b63d0f3c91bd233ebee20
Anthony N <foto.arn@gmail.com>
2020-10-21 23:21:09 -0700

    + Imports & window setup

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from tkinter import *
+import base64
 
+# Tk() initialized tkinter which means window created
+root = Tk()
+# geometry() set the width and height of the window
+root.geometry('500x300')
+# resizable(0,0) set the fixed size of the window
+root.sizable(0,0)
+# title() set the title of the window
+root.title("encode & decode, son")
+
+
+ 
