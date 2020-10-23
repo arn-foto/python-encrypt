b7fdfcd28d2acad148db556db049737bd4781b7f
Anthony N <foto.arn@gmail.com>
2020-10-22 20:56:06 -0700

    + label

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter
 import base64
 
 # Tk() initialized tkinter which means window created
@@ -9,6 +9,16 @@ root.geometry('500x300')
 root.sizable(0,0)
 # title() set the title of the window
 root.title("encode & decode, son")
+
+# Label() widget use to display
+#  one or more than one line of text that users aren’t able to modify.
+label(root, test = 'python encrypt', font = 'arial 20 bold').pack()
+# "root" is the name we call our window
+# "text" is displayed on the label
+# "pack" organized widget in block
+label(root, text = 'excrypt python', font = 'arial 20 bold').pack(side=BOTTOM)
+
+
 
 
  
