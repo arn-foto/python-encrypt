1292a1d13dd9ed575d3abc1b09102e08e1069024
Anthony N <foto.arn@gmail.com>
2020-10-22 21:09:33 -0700

    + encode function

--- main.py
+++ main.py
@@ -18,11 +18,26 @@ label(root, test = 'python encrypt', font = 'arial 20 bold').pack()
 # "pack" organized widget in block
 label(root, text = 'excrypt python', font = 'arial 20 bold').pack(side=BOTTOM)
 
-# here is where I will define the variables
+# Here is where I will define the variables:
+# "Text" variable stores the message to encode and decode
+# "private_key" variable store the private key used to encode and decode
+# "mode" is used to select that is either encoding or decoding
+# "Result" store the result
 Text = Stringvar()
 private_key = Stringvar()
 mode = Stringvar()
 Result = Stringvar()
+
+# This is the function to encode the message
+
+def encode(key, message):
+    enc = []
+    
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
 
 
 
